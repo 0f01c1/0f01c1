94e6b009de1983610dcded1049ee05b06105d6f8
Harry Thomas <harry.thomas.17@ucl.ac.uk>
2025-11-19 21:58:20 +0000

    Add astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,11 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import expressiveCode from "astro-expressive-code";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://0f01c1.com',
-    integrations: [mdx(), sitemap(), tailwind()],
-    markdown: {
-        shikiConfig: {
-          // Choose from Shiki's built-in themes (or add your own)
-          // https://shiki.style/themes
-          // Alternatively, provide multiple themes
-          // See note below for using dual light/dark themes
-          themes: {
-            light: 'poimandres',
-            dark: 'catppuccin-latte',
-          },
-        },
-      },
-    
-})
+  site: 'https://0f01c1.com',
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind()]
+});
